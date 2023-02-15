6b492da1e7489b46d168406542e09ebb59e235e2
Anh Nhat <phananhnhat2@gmail.com>
2023-02-15 13:24:48 +0700

    .

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func init() {
 	)
 
 	server = gin.Default()
-	// server.Use(cors.Default())
+	server.Use(CORSMiddleware())
 }
 
 func main() {
@@ -51,4 +51,20 @@ func main() {
 	routes.MemberRoutes(router)
 
 	server.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
