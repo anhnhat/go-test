9feea8b52f6c851191d41cde913961d66c1f1818
Anh Nhat <phananhnhat2@gmail.com>
2023-03-17 18:23:09 +0700

    remove old

--- models/role.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type Role struct {
-	gorm.Model
-	Name    string    `gorm:"uniqueIndex,not null"`
-	Members []*Member `gorm:"many2many:member_roles;"`
-}
--- models/team.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type Team struct {
-	gorm.Model
-	Name string `gorm:"type:varchar(255);not null"`
-}
